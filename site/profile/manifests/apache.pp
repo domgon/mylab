9f2c9ae6618b81edc7359a81a00814a5d5296272
Domingos Gonçalves <32734693+domgon@users.noreply.github.com>
2022-03-14 21:30:24 +0000

    Update apache.pp

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -2,6 +2,13 @@ class profile::apache {
   package {'httpd':
     ensure => present,
   }
+  
+  firewall { '100 allow http and https access':
+    dport  => [80, 443],
+    proto  => 'tcp',
+    action => 'accept'
+  }
+  
   service {'httpd':
     ensure => 'running',
     enable => 'true',
