2b881ea32f02e27087c5d51e32a0ede2aafeeccc
Domingos Gonçalves <32734693+domgon@users.noreply.github.com>
2022-03-14 21:58:04 +0000

    Update mariadb.pp

--- site/profile/manifests/mariadb.pp
+++ site/profile/manifests/mariadb.pp
@@ -6,4 +6,11 @@ class profile::mariadb {
     ensure => 'running',
     enable => 'true'
   }
+  include ::firewall
+  
+  firewall { '100 allow mysql access':
+    dport  => [3306],
+    proto  => 'tcp',
+    action => 'accept'
+  }
 }
