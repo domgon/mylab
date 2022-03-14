105a7b848f324f9f305003ef6168e6296396d6c3
Domingos Gonçalves <32734693+domgon@users.noreply.github.com>
2022-03-14 17:04:41 +0000

    Update commun.pp

--- site/profile/manifests/commun.pp
+++ site/profile/manifests/commun.pp
@@ -1,9 +1,13 @@
 class profile::commun {
-  package {'net-tools':
-    ensure => present,
+  package { 'net-tools':
+          ensure => present,
   }
-  package {'bind-utils':
-    ensure => present,
+  package { 'bind-utils':
+          ensure => present,
+  }
+  service { 'puppet':
+          ensure => 'running',
+          enable => 'true',
   }
   include profile::openssh
 }
