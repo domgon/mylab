915bfe2ec9c2e9ebc88560ce90e46df30a0cb94c
Domingos Gonçalves <32734693+domgon@users.noreply.github.com>
2022-03-15 12:19:01 +0000

    Update selinux.pp

--- site/profile/manifests/selinux.pp
+++ site/profile/manifests/selinux.pp
@@ -1,6 +1,12 @@
 class profile::selinux {
 
-  class { selinux:
-    mode => 'disabled',
+#  class { selinux:
+#    mode => 'disabled',
+#  }
+
+  include ::selinux
+  
+  selinux::module { 'disable':
+    mode => 'disabled'
   }
 }
